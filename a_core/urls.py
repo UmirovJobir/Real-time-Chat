675f0b60bc5022c94300a035628c3c87b8266325
UmirovJobir <jobirumirovmoliya@gmail.com>
2024-04-20 18:52:52 +0500

    Ready to deploy

--- a_core/urls.py
+++ a_core/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('@<username>/', profile_view, name="profile"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
 # Only used in development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
