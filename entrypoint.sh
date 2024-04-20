c2d4223acf91aa93d9473ccd60aeb66cef34c720
UmirovJobir <jobirumirovmoliya@gmail.com>
2024-04-20 18:30:33 +0500

    Ready to deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,12 @@
 echo "Apply database migrations"
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:5000 --insecure 
 
-daphne a_core.asgi:application -b 0.0.0.0:5000 
+# gunicorn imodels.wsgi:application --bind 0.0.0.0:8000 --timeout 300
+
+# echo "Collect static files"
+# python3 manage.py collectstatic
 
 
 exec "$@"
