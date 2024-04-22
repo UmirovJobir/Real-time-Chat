759b0d795e0792e5865b9863cc19846d4ba7d34d
UmirovJobir <jobirumirovmoliya@gmail.com>
2024-04-22 15:17:27 +0500

    Private chat

--- a_core/settings.py
+++ a_core/settings.py
@@ -98,8 +98,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            # "hosts": [("localhost", 6379)],
-            "hosts": [("redis", 6379)],
+            "hosts": [("localhost", 6379)],
+            # "hosts": [("redis", 6379)],
         },
     },
 }
