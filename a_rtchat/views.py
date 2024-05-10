db8a052038a3ecdab97f55636aca4d2029f2d321
UmirovJobir <jobirumirovmoliya@gmail.com>
2024-05-10 18:12:37 +0500

    Group Chat half part

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.contrib.auth.models import User
 from .models import ChatGroup, GroupMessage
-from .forms import ChatmessageCreateForm
+from .forms import ChatmessageCreateForm, NewGroupForm
 
 
 @login_required
@@ -20,6 +20,10 @@ def chat_view(request, chatroom_name='public-chat'):
             if member != request.user:
                 other_user = member
                 break
+        
+    if chat_group.groupchat_name:
+        if request.user not in chat_group.members.all():
+            chat_group.members.add(request.user)
     
     if request.htmx:
         form = ChatmessageCreateForm(request.POST)
@@ -38,7 +42,8 @@ def chat_view(request, chatroom_name='public-chat'):
         'chat_messages': chat_messages,
         'form': form,
         'other_user': other_user,
-        'chatroom_name': chatroom_name
+        'chatroom_name': chatroom_name,
+        'chat_group': chat_group
     }
     
     return render(request, 'a_rtchat/chat.html', context=context)
@@ -65,3 +70,22 @@ def get_or_create_chatroom(request, username):
         chatroom.members.add(other_user, request.user)
     
     return redirect('chatroom', chatroom.group_name)
+
+
+@login_required
+def create_groupchat(request):
+    form = NewGroupForm()
+    
+    if request.method == 'POST':
+        form = NewGroupForm(request.POST)
+        if form.is_valid():
+            new_groupchat = form.save(commit=False)
+            new_groupchat.admin = request.user
+            new_groupchat.save()
+            new_groupchat.members.add(request.user)
+            return redirect('chatroom', new_groupchat.group_name)
+            
+    context = {
+        'form': form
+    }
+    return render(request, 'a_rtchat/create_groupchat.html', context=context)
