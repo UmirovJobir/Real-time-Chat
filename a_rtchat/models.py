d670db3390feaf66b9df98914ea15bb73fd08297
UmirovJobir <jobirumirovmoliya@gmail.com>
2024-05-15 14:13:03 +0500

    Sending Files - Part 9

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-import shortuuid
+import shortuuid, os
 
 
 class ChatGroup(models.Model):
@@ -17,14 +17,33 @@ class ChatGroup(models.Model):
 class GroupMessage(models.Model):
     group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.CharField(max_length=300)
+    body = models.CharField(max_length=300, blank=True, null=True)
+    file = models.FileField(upload_to='files/', null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     
+    @property
+    def filename(self):
+        if self.file:
+            return os.path.basename(self.file.name)
+        else:
+            return None
+    
     def __str__(self):
-        return f'{self.author.username} : {self.body}'
+        if self.body:
+            return f'{self.author.username} : {self.body}'
+        elif self.file:
+            return f'{self.author.username} : {self.filename}'
 
     class Meta:
         ordering = ['-created']
+    
+    @property
+    def is_image(self):
+        if self.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.svg', '.webp')):
+            return True
+        else:
+            return False
+
 
 # class UserChannel(models.Model):
 #     member = models.ForeignKey(User, on_delete=models.CASCADE)
